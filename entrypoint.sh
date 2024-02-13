2907506ff063c57f48922c5950c94c4de343c498
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2024-02-12 21:39:56 -0500

    Added route to docker network
    This allows adding second network route to access docker DNS
    
    Signed-off-by: TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ set -eux
 if [ $OVERRIDE_DEFAULT_ROUTE = "true" ]; then
   ip route delete default
   ip route add default via $GATEWAY_IP dev $LAN_NIC
+  if [ $TRUENAS_SYSTEM ] = "true" ]; then
+    ip route add $TRUNENAS_SERVICE_NETWORK via $TRUENAS_CLUSTER_GATEWAY_IP dev eth0
+  fi
 fi
 
 tailscaled --tun=$TSD_TUN --statedir=/config/tailscale --port=${TSD_PORT} ${TSD_EXTRA_ARGS} &
